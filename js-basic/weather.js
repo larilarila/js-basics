12351954c46a8303e2c6df0c5e87f2c2777b9858
larilarila <larilarila@naver.com>
2019-10-13 16:51:46 +0900

    getting weather api

--- js-basic/weather.js
+++ js-basic/weather.js
@@ -1,7 +1,21 @@
 const weather = document.querySelector(".js-weather");
 
-const API_KEY = "c17c24a90517e61e3312a10c5525e17e";
+const API_KEY = "c17c24a90517e61e3312a10c5525e17e"; //컴끼리 소통하려고 만든 것
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  ) //따옴표가 아니라 ``를 사용해야함
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    }); // then은 데이터가 완전히 들어온 후에 함수 호출함
+} //메일이 새로고침없이 데이터 들어오는 것은 얘때문
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,9 +27,10 @@ function handleGeoSucces(position) {
   const coordsObj = {
     latitude,
     longitude
-  };
+  }; //이름이 같으면 이렇게 적어도 됨
   saveCoords(coordsObj);
-}
+  getWeather(latitude, longitude);
+} //handleGeoSucces는 local storage에 좌표값이 없으면 getWeather함수가 실행됨
 
 function handleGeoError() {
   console.log("Cant access geo location");
@@ -30,7 +45,8 @@ function loadCoords() {
   if (loadedCoords == null) {
     askForCoords();
   } else {
-    // getWeather
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 
